1d64b5bfc4cf18e94e6da0df1634563ad3ca2727
Daniel Garcia <dgarcia885@alumno.uned.es>
2018-07-01 18:24:26 +0000

    Log path is now relative to HttpServer.py script path

--- HttpServer.py
+++ HttpServer.py
@@ -1,11 +1,14 @@
+# - *- coding: utf- 8 - *-
 '''
 Created on 10/11/2015
+Modified on 2018/07/01
 
 @author: jcsombria
 '''
 import cherrypy
 import ujson
 import time
+import os
 from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
 from ws4py.websocket import WebSocket, EchoWebSocket
 
@@ -25,10 +28,16 @@ class Root(object):
     return response.encode("utf-8")
 
 if __name__ == '__main__':
+  # Provides a relative log path from the original script path
+  # instead from the invocation path
+  base_dir = os.path.dirname(os.path.realpath(__file__))
+  log_dir = os.path.join(base_dir, 'log')
+  access_log_file = os.path.join(log_dir, 'access.log')
+  error_log_file = os.path.join(log_dir, 'error.log')
   cherrypy.config.update({
     'server.socket_port': 2055,
-    'log.access_file' : './log/access.log',
-    'log.errors_file' : './log/error.log'
+    'log.access_file' : access_log_file,
+    'log.errors_file' : error_log_file
   })
   config = {
     '/': {
