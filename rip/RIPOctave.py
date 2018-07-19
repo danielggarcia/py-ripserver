7665196bef98568db10a8d887f80275d0d0a75a7
Daniel Garcia <contacto@danigarcia.org>
2018-07-19 18:04:03 +0000

    Data interchange between ejss and RIP Server working

--- rip/RIPOctave.py
+++ rip/RIPOctave.py
@@ -18,35 +18,35 @@ class RIPOctave(RIPGeneric):
     self.octavePath = '/home/pi/workspace/robot/octave'
     self.dataArray = []
     self.messageArray = {}
-    self.TS = 0
-    self.DT = 0
-    self.CD = 0
-    self.CI = 0
-    self.MP = 0
-    self.MS = 0
-    self.IrF = 0
-    self.IrR = 0
-    self.IrL = 0
-    self.Mensaje = ""
+    self.TS = 0.0
+    self.DT = 0.0
+    self.CD = 0.0
+    self.CI = 0.0
+    self.MP = 0.0
+    self.MS = 0.0
+    self.IrF = 0.0
+    self.IrR = 0.0
+    self.IrL = 0.0
+    self.previousMessage = []
+    
+    octave.eval('global dataArray;')
+    octave.eval('global messageArray;')
+    octave.eval('global TS;')
+    octave.eval('global DT;')
+    octave.eval('global CD;')
+    octave.eval('global CI;')
+    octave.eval('global MP;')
+    octave.eval('global MS;')
+    octave.eval('global IrF;')
+    octave.eval('global IrR;')
+    octave.eval('global IrL;')
 
-    octave.eval('global dataArray = [];')
-    octave.eval('global messageArray = {};')
-    octave.eval('global TS = 0;')
-    octave.eval('global DT = 0;')
-    octave.eval('global CD = 0;')
-    octave.eval('global CI = 0;')
-    octave.eval('global MP = 0;')
-    octave.eval('global MS = 0;')
-    octave.eval('global IrF = 0;')
-    octave.eval('global IrR = 0;')
-    octave.eval('global IrL = 0;')
-    octave.eval('global Mensaje = "";')
-    octave.eval('global currentAction = "";')
-    octave.eval('global octaveCode = "";')
-
+    #octave.eval('global currentAction;')
+    octave.eval('global octaveCode;')
+    octave.eval('global debugLevel;')
+    
     try:
-      octave.addpath(self.octavePath)
-      octave.robotSetup()
+      self.initSession()
     except:
       pass
 
@@ -123,6 +123,14 @@ class RIPOctave(RIPGeneric):
         'precision':'0'
       })
     self.readables.append({
+        'name':'TSM',
+        'description':'Timestamp of the message reading',
+        'type':'long',
+        'min':'-Inf',
+        'max':'Inf',
+        'precision':'0'
+      })
+    self.readables.append({
         'name':'Mensaje',
         'description':'Last readed message',
         'type':'str',
@@ -150,21 +158,31 @@ class RIPOctave(RIPGeneric):
   def __del__(self):
     octave.endSession()
 
+  # This method will be invoked from HttpServer.SSE
+  def start(self):
+    octave.addpath(self.octavePath)
+    octave.robotSetup()
+    octave.arduinoProcessInputs("K")
+
   def set(self, expid, variables, values):
     '''
     Writes one or more variables to the workspace of the current Octave session
     '''
     n = len(variables)
-    #f = open('/var/log/robot/ejss.log','a')
-    #f.write('[SET]: Expid(' + expid + ") Variables(" + str(variables) + ") Values(" + str(values) + ")")
-    #f.close
+    f = open('/var/log/robot/ejss.log','a')
+    f.write('[SET]: Expid(' + expid + ") Variables(" + str(variables) + ") Values(" + str(values) + ")")
+    
     for i in range(n):
       try:
-        octave.push(variables[i], values[i])
-        octave.arduinoProcessInputs()
-      except:
+        f.write('[SET]: Variable: ' + str(variables[i] + "; value: " + str(values[i])))
+        if(variables[i] == 'currentAction'):
+          octave.arduinoProcessInputs(values[i])
+        elif(variables[i] == 'octaveCode'):
+          octave.executeOctaveCode(values[i])
+      except Exception as e:
+        f.write("Error al realizar el SET: " + str(e))
         pass
-
+    f.close
   def get(self, expid, variables):
     '''
     Retrieve one or more variables from the workspace of the current Octave session
@@ -178,18 +196,64 @@ class RIPOctave(RIPGeneric):
       try:
         toReturn[name] = octave.pull(name)
         f.write('[GET]: Expid(' + expid + ") Variables(" + str(variables) + ") toReturn(" + str(toReturn) + ")\n")
-        f.close
+        f.close()
       except:
         pass
     return toReturn
 
   def getValuesToNotify(self):
-    lastData = octave.getLastData()
-    returnValue = [
-      ['time', 'TS', 'DT', 'CD', 'CI', 'MP', 'MS','IrF','IrR','IrL','Mensaje'],
-      [self.sampler.lastTime(), lastData[0], lastData[1], lastData[2], lastData[3], lastData[4], lastData[5], lastData[6], lastData[7], lastData[8], 'Mensaje']
-    ]
     f = open('/var/log/robot/ejss.log','a')
-    f.write("[getValuesToNotify]: Variables(" + str(returnValue) + ")\n")
-    f.close
+    returnValue = self.previousMessage
+    try:
+      self.TS = 0
+      self.DT = 0
+      self.CD = 0
+      self.CI = 0
+      self.MP = 0
+      self.MS = 0
+      self.IrF = 0
+      self.IrR = 0
+      self.IrL = 0
+      result = octave.updateGlobals()
+      f.write("Result: " + str(type(result)) + ", " + str(result) + "\n")
+      if type(result) is list:
+        pass
+      elif type(result) is np.ndarray and result.size == 9:
+        if type(result.item(0)) is float:
+          self.TS = result.item(0)
+        if type(result.item(1)) is float:
+          self.DT = result.item(1)
+        if type(result.item(2)) is float:
+          self.CD = result.item(2)
+        if type(result.item(3)) is float:
+          self.CI = result.item(3)
+        if type(result.item(4)) is float:
+          self.MP = result.item(4)
+        if type(result.item(5)) is float:
+          self.MS = result.item(5)
+        if type(result.item(6)) is float:
+          self.IrF = result.item(6)
+        if type(result.item(7)) is float:
+          self.IrR = result.item(7)
+        if type(result.item(8)) is float:
+          self.IrL = result.item(8)
+      try:
+        msg = octave.getLastMessage()
+        if msg.size == 2:
+          self.TSM = int(msg.item(0))
+          self.Mensaje = msg.item(1)
+      except Exception as e:
+        f.write("Error retrieving messages from server: " + str(e))
+        self.TSM = 0
+        self.Mensaje = ""
+        pass
+      returnValue = [
+        ['time', 'TS', 'DT', 'CD', 'CI', 'MP', 'MS','IrF','IrR','IrL', 'TSM', 'Mensaje'],
+        [self.sampler.lastTime(), self.TS, self.DT, self.CD, self.CI, self.MP, self.MS, self.IrF, self.IrR, self.IrL, self.TSM, self.Mensaje]
+      ]
+      self.previousMessage = returnValue
+      f.close()
+    except Exception as e:
+      returnValue = [['id', 'Mensaje'], [-1, str(e)]]
+      f.close()
     return returnValue
